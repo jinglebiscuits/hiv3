a1a2db8ddb015cd8fd2a77e5afafbc853f110129
jinglebiscuits <scott.wehby@gmail.com>
2015-09-12 15:07:24 -0500

    This fixes #2 - syncing character info with item count.

---
+++ Assets/Scripts/CharacterInfoSceneManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterInfoSceneManager : MonoBehaviour {
+
+	public Text toolsCount;
+	public Text clothesCount;
+	public Text booksCount;
+	public Text gamesCount;
+	public Text condomsCount;
+	public Text birthControlCount;
+	public Text sportsEquipmentCount;
+	public Text songCount;
+	public Text flowersCount;
+	
+	private Dictionary<string, Text> itemLookup = new Dictionary<string, Text>();
+	
+	// Use this for initialization
+	void Start () {
+		Person person = Player.player.FocusedPerson;
+		itemLookup.Add("Tools", toolsCount);
+		itemLookup.Add("Clothes", clothesCount);
+		itemLookup.Add("Books", booksCount);
+		itemLookup.Add("Games", gamesCount);
+		itemLookup.Add("Condoms", condomsCount);
+		itemLookup.Add("Birth Control", birthControlCount);
+		itemLookup.Add("Sports Equipment", sportsEquipmentCount);
+		itemLookup.Add("Songs", songCount);
+		itemLookup.Add("Flowers", flowersCount);
+		
+		foreach (Item item in person.BaseItems.items) {
+			if (itemLookup.ContainsKey(item.Name)) {
+				itemLookup[item.Name].text = 
+				person.QualitiesDict[item.Name].Level.ToString();
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Qualities/Items/BaseItems.cs
+++ Assets/Scripts/Qualities/Items/BaseItems.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿//Updates to this list require an update
+//to the xml schema and xml files.
+using System;
 
 [Serializable]
 public class BaseItems {
 
-	public Item coins = new Item("Coins", "Buy stuff with coins.", 0, 0, 1, false);
+	public Item coins = new Item("Coins", "Buy stuff with coins.", 100, 0, 1, false);
 	public Item tools = new Item("Tools", "Get paid", 0, 0, 100, false);
 	public Item clothes = new Item("Clothes", "Looking good", 0, 0, 60, false);
 	public Item books = new Item("Books", "Who needs tv?", 0, 0, 10, false);
@@ -12,11 +14,13 @@ public class BaseItems {
 	public Item condoms = new Item("Condoms", "Way to go", 0, 0, 3, false);
 	public Item birthControl = new Item("Birth Control", "That's the ticket", 0, 0, 2, false);
 	public Item sportsEquipment = new Item("Sports Equipment", "Level up!", 0, 0, 30, false);
+	public Item songs = new Item("Songs", "Level up!", 0, 0, 1, false);
+	public Item flowers = new Item("Flowers", "Level up!", 0, 0, 10, false);
 	
 	public Item[] items;
 	
 	public BaseItems()
 	{
-		items = new Item[]{coins, tools, clothes, books, games, cellPhone, condoms, birthControl, sportsEquipment};
+		items = new Item[]{coins, tools, clothes, books, games, cellPhone, condoms, birthControl, sportsEquipment, songs, flowers};
 	}
 }
